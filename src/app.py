d5cdf4bd9e2bbbc99f4dbb81047c678aedee2902
Luis Eloy Homobono Fuentes <luis.homobono@ciencias.unam.mx>
2022-07-20 14:48:22 -0500

    Rutas de enlaces cortos

--- src/app.py
+++ src/app.py
@@ -28,10 +28,17 @@ def create_url_short():
 
             while True:
                 short_url = shortuuid.ShortUUID().random(length=7)
-                cursor.execute('SELECT * FROM enlaces WHERE enlace_corto = BINARY %s', (short_url))
+                cursor.execute("SELECT * FROM enlaces WHERE enlace_corto = BINARY %s", (short_url))
 
                 if not cursor.fetchone():
                     break
+
+            cursor.execute("SELECT enlace_corto FROM enlaces WHERE url = BINARY %s", (url,))
+
+            data = cursor.fetchone()
+            if data:
+                new_url = endpoint + "/" + data[0]
+                return jsonify(respuesta=new_url), 200
 
             cursor.execute("INSERT INTO enlaces (url, enlace_corto) VALUES (%s, %s)", (url, short_url))
 
@@ -39,8 +46,22 @@ def create_url_short():
             cursor.close()
 
             new_url = endpoint + "/" + short_url
-            return jsonify(respuesta=new_url)
+            return jsonify(respuesta=new_url), 200
     except Exception as e:
+        return jsonify(respuesta=f"Error en petición {e}"), 500
+
+@app.route("/<id>")
+def get_url(id):
+    try:
+        cursor = mysql.connection.cursor()
+
+        cursor.execute("SELECT url FROM enlaces WHERE enlace_corto = BINARY %s", (id,))
+        data = cursor.fetchone()
+
+        cursor.close()
+
+        return jsonify(respuesta=data[0]), 200
+    except:
         return jsonify(respuesta=f"Error en petición {e}"), 500
 
 if __name__ == "__main__":
